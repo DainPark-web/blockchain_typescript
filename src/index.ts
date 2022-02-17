224759377ebe7e237d3bfeb52a7371feb6d78633
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-17 23:33:36 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJs from "crypto-js";
+
 class Block {
     public index: number;
     public hash : string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockhash = (
+        index:number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string
+        ): string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(
         index: number,
@@ -26,7 +35,12 @@ class Block {
 
 const genessisBlock:Block = new Block(0, "20202020202020202", "", "block1234", 123456);
 
-let blockchain: [Block] = [genessisBlock];
+let blockchain: Block[] = [genessisBlock];
 
+const getBlockchain = ():Block[] => blockchain;
+
+const getLatesBlock = (): Block => getBlockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime()/1000);
 
 console.log(blockchain)
