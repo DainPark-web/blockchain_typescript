3648c67f1d4ec5836b6a42dae9487a22af905ac2
dain <57316771+DainPark-web@users.noreply.github.com>
2022-02-17 23:18:32 +0100

    block

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,22 @@ class Block {
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    constructor(
+        index: number,
+        hash : string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+        ){
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
+    }
+
+
+
+    
 }
